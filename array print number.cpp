bd646722e9a0ef6b8b9cec386c1f099e8f5a6091
pradeep sahu <57285880+Pradeep8842@users.noreply.github.com>
2020-02-02 16:39:27 +0530

    Add files via upload

---
+++ array print number.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	int  marks[5];
+	marks[0]=50;
+	marks[1]=60;
+	marks[2]=70;
+	marks[3]=80;
+	marks[4]=90;
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n,",marks[i]);
+	}
+	getch();
+	}
